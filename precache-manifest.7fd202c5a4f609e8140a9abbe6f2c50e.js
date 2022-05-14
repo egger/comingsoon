b2cc9602e5ae0af66b6f335078e8d561fb4c78cc
Thomas Egger <thomas@uperox.com>
2022-05-14 11:34:31 +0200

    📁Uploaded files

---
+++ precache-manifest.7fd202c5a4f609e8140a9abbe6f2c50e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d58b0f20b275ac41cad7bd4e56bd0923",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ee07015855417751188",
+    "url": "/static/css/main.e200caae.chunk.css"
+  },
+  {
+    "revision": "5dea356e92a012319176",
+    "url": "/static/js/2.7c74887b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c74887b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ee07015855417751188",
+    "url": "/static/js/main.49f5bf4a.chunk.js"
+  },
+  {
+    "revision": "f1e7320e81484d0c7ee0",
+    "url": "/static/js/runtime-main.5b90ac66.js"
+  },
+  {
+    "revision": "5159a5d89abe8bf68b09b569dbeccbc0",
+    "url": "/static/media/IBMPlexSans-Bold.5159a5d8.ttf"
+  },
+  {
+    "revision": "ee425cc83f37323665790c89758cf359",
+    "url": "/static/media/IBMPlexSans-BoldItalic.ee425cc8.ttf"
+  },
+  {
+    "revision": "40bbef08ca6f6edea2a9a9e882541ce0",
+    "url": "/static/media/IBMPlexSans-Italic.40bbef08.ttf"
+  },
+  {
+    "revision": "ee83103a4a777209b0f759a4ff598066",
+    "url": "/static/media/IBMPlexSans-Medium.ee83103a.ttf"
+  },
+  {
+    "revision": "eb7dadea8e7c37ce1a1406045dda7c1e",
+    "url": "/static/media/IBMPlexSans-MediumItalic.eb7dadea.ttf"
+  },
+  {
+    "revision": "c02b4dc6554c116e4c40f254889d5871",
+    "url": "/static/media/IBMPlexSans-Regular.c02b4dc6.ttf"
+  },
+  {
+    "revision": "50f13391babe5b650b8a0589dd962f74",
+    "url": "/static/media/facebook.50f13391.svg"
+  },
+  {
+    "revision": "baf48a32fd45986dfb5b52aada1b558a",
+    "url": "/static/media/github.baf48a32.svg"
+  },
+  {
+    "revision": "a50716dd845ea43407a9a1bcffe42aef",
+    "url": "/static/media/twitter.a50716dd.svg"
+  }
+]);
